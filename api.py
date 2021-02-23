616525ea7ad2006c3c171e6933426b83ddea61bf
k-abhi1512 <2akj1018@gmail.com>
2021-02-23 18:43:56 +0530

    module to generate qr code from product inventory

---
+++ api.py
@@ -0,0 +1,57 @@
+from app import *
+
+
+# route to get all products
+@app.route('/products-detail', methods=['GET'])
+def get_products():
+    '''Function to get all the products in the database'''
+    return jsonify({'Inventory': Inventory.get_all_products()})
+
+
+# route to get product by Inventory_key
+@app.route('/product-detail/<Inventory_key>', methods=['GET'])
+def get_product_by_id(Inventory_key):
+    return_value = Inventory.get_product(Inventory_key)
+    return jsonify(return_value)
+
+
+# route to add new product
+@app.route('/add/products', methods=['POST'])
+def add_product():
+    '''Function to add new product to our database'''
+    request_data = request.get_json()  # getting data from client
+    Inventory.add_product(request_data["Calling_Function"], request_data["Manufacture_Date"],
+                    request_data["Product_Id"],request_data["Batch_Id"])
+    response = Response("Product Details added!", 201, mimetype='application/json')
+    return response
+
+
+# route to update product with PUT method
+@app.route('/product/<Inventory_key>', methods=['PUT'])
+def update_product(Inventory_key):
+    '''Function to edit product data in our database using Inventory_key'''
+    request_data = request.get_json()
+    Inventory.update_product(Inventory_key, request_data['Calling_Function'], request_data['Manufacture_Date'], request_data['Product_Id'], request_data['Batch_Id'])
+    response = Response("Product Details Updated!", status=200, mimetype='application/json')
+    return response
+
+
+# route to delete movie using the DELETE method
+@app.route('/movies/<Inventory_key>', methods=['DELETE'])
+def remove_product(Inventory_key):
+    '''Function to delete product details from our database'''
+    Inventory.delete_product(Inventory_key)
+    response = Response("Product Data Deleted!", status=200, mimetype='application/json')
+    return response
+
+
+# Generate the QR Code Image from inventory_key and return the image in .svg or .png
+@app.route('/unique_qr_code/q=<Inventory_key>', methods=['GET'])
+def get_qr_code(Inventory_key):
+    return_value = Inventory.get_qr_code(Inventory_key)
+    return jsonify(return_value)
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ app.py
@@ -0,0 +1,103 @@
+from flask import Flask, request, jsonify, make_response, render_template
+from flask_mysqldb import MySQL
+from flask_cors import CORS, cross_origin
+from sqlalchemy.sql import func
+ 
+
+app = Flask(__name__)
+CORS(app)
+
+UPLOAD_FOLDER = 'static/uploads/'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/en_inventory_details'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+#Our DataBase Model
+class Inventory(db.Model):
+    Inventory_key = db.Column(db.Integer, autoincrement= 1000000000000, primary_key=True)
+    Row_Creation_Date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    Row_update_Date = db.Column(db.DateTime(timezone=True), onupdate=func.now())
+    Calling_Function = db.Column(db.String(20))
+    Manufacture_Date = db.Column(db.DateTime())
+    Product_Id = db.Column(db.String(20))
+    Batch_Id = db.Column(db.String(20))
+
+    #function will help us display our output as JSON.
+    def json(self):
+        return {'Inventory_key': self.Inventory_key, 'Calling_Function': self.Calling_Function, 'Manufacture_Date': self.Manufacture_Date, 'Product_Id': self.Product_Id, 'Batch_Id':self.Batch_Id}
+
+
+    #Add products
+    def add_product(_Calling_Function, _Manufacture_Date, _Product_Id, _Batch_Id):
+        '''function to add product to database'''
+        # creating an instance of our Product constructor
+        new_product = Inventory(Calling_Function=_Calling_Function, Manufacture_Date=_Manufacture_Date, Product_Id=_Product_Id, Batch_Id=_Batch_Id)
+        db.session.add(new_product)  # add new product details to database session
+        db.session.commit()  # commit changes to session
+
+
+    # Get All Product details
+    def get_all_products():
+        '''function to get all products in our database'''
+        return [Inventory.json(pr) for pr in Inventory.query.all()]
+
+    #Get single product details
+    def get_product(_Inventory_key):
+        '''function to get product details using the Inventory_key of the product as parameter'''
+        return [Inventory.json(Inventory.query.filter_by(Inventory_key=_Inventory_key).first())]
+        # coverts our output to the json format defined earlier
+        # the filter_by method filters the query by the Inventory_key
+        # since our key is unique we will only get one result
+        # the .first() method will get that first value returned as unique and only value
+    
+    #function to update product on our database
+    def update_product(_Inventory_key, _Calling_Function, _Manufacture_Date, _Product_Id, _Batch_Id):
+        '''function to update the details of a product using parameters'''
+        product_to_update = Inventory.query.filter_by(Inventory_key=_Inventory_key).first()
+        product_to_update.Calling_Function = _Calling_Function
+        product_to_update.Manufacture_Date = _Manufacture_Date
+        product_to_update.Product_Id = _Product_Id
+        product_to_update.Batch_Id = _Batch_Id
+        db.session.commit()
+
+    #Delete product
+    def delete_product(_Inventory_key):
+        '''function to delete a product from our database using
+           the id of the product as a parameter'''
+        Inventory.query.filter_by(Inventory_key=_Inventory_key).delete()
+        # filter id and delete
+        db.session.commit()  # commiting the new change to our database
+
+    #Get the qr code
+    def get_qr_code(_Inventory_key):
+
+        key = Inventory.query.filter_by(Inventory_key=Inventory_key).first()
+
+        if not key:
+            return jsonify({'message' : 'No data found!'})
+
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,                                                      
+            )
+
+        qr.add_data(key.Inventory_key)
+
+        qr.make(fit=True)
+        img = qr.make_image(fill_color="black", back_color="white")
+
+        val = '' #get value as svg to get .svg image(modify the function as per need)
+
+        if val == svg:
+            filename = ''.join(random.choice(string.ascii_lowercase) for _ in range(7))+'.svg'
+        else:
+            filename = ''.join(random.choice(string.ascii_lowercase) for _ in range(7))+'.png'
+        
+        img.save(filename)
+        new_qr = {'QR_Code_Image': img}
+
+        return jsonify({'new_qr': new_qr})
